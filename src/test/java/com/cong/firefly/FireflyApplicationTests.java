2653515b35fb4b937781860e61c35b51ef414518
cong <congco@foxmail.com>
2023-01-16 16:20:28 +0800

    feat:add

---
+++ src/test/java/com/cong/firefly/FireflyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cong.firefly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FireflyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
